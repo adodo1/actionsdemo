4b16d4ff09ded734f7c91b95fb8cd0122a20e761
dodo <adodo1@126.com>
2020-12-28 09:26:38 +0800

    requests test

--- hello.py
+++ hello.py
@@ -1,8 +1,14 @@
 # -*- coding: utf-8 -*-
-import io, sys, os, time
+import io, sys, os, time, requests
 
 
 if __name__ == '__main__':
     #
     print('Hello DoDo!')
     print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+    # 
+    url = 'http://dzdy.itzjj.cn/static/skin/summer/images/top1.jpg'
+    r = requests.get(url)
+    data = r.content
+    print(r.status_code)
+    print(len(data))
