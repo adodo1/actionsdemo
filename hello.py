f0821ce1c0da58146b296f0196f76e073c29b914
dodo <adodo1@126.com>
2020-12-28 09:22:57 +0800

    add time alter

--- hello.py
+++ hello.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
-import io, sys, os
+import io, sys, os, time
 
 
 if __name__ == '__main__':
     #
     print('Hello DoDo!')
+    print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
