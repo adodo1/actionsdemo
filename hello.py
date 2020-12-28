6de8cd879b28b3e93c0b134ed6da7878cf655539
dodo <adodo1@126.com>
2020-12-28 09:32:22 +0800

    html test

--- hello.py
+++ hello.py
@@ -8,7 +8,8 @@ if __name__ == '__main__':
     print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
     # 
     url = 'http://dzdy.itzjj.cn/static/skin/summer/images/top1.jpg'
+    url = 'https://avgle.com/videos'
     r = requests.get(url)
     data = r.content
     print(r.status_code)
-    print(len(data))
+    print(data)
